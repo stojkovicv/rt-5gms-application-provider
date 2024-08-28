003a6d125a2e9ba87e9f05e9f55bd93870484df6
stepski <vukbg.stojkovic@gmail.com>
2024-08-28 18:57:00 +0200

    test master cicd

--- management-ui/server.py
+++ management-ui/server.py
@@ -6,7 +6,7 @@ For full license terms please see the LICENSE file distributed with this
 program. If this file is missing then the license can be retrieved from
 https://drive.google.com/file/d/1cinCiA778IErENZ3JN52VFW-1ffHpx7Z/view
 '''
-# Commit
+
 import os
 import json
 import requests
